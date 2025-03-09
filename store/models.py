5adeda3033d00476cc789f1377bc2ca50221841e
iammanishanand <manishanand869@gmail.com>
2025-03-09 12:01:57 +0530

    phase 4 complete

--- store/models.py
+++ store/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural = "Categories"
 
 # Customer Model
 class Customer(models.Model):
--- store/views.py
+++ store/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
 def home(request):
-    return render(request, 'home.html', {})
+    products = Product.objects.all()  # Fetch all products
+    context = {'products': products}  # Add to context
+    return render(request, 'home.html', context)
