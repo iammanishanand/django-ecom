8494c360f4894962ed024a2e69b6d3f97ec9b97d
iammanishanand <manishanand869@gmail.com>
2025-03-13 14:31:00 +0530

    Add Product To Cart

--- cart/cart.py
+++ cart/cart.py
@@ -9,3 +9,15 @@ class Cart():
             cart =self.session['session_key'] = {}
 
         self.cart =cart
+
+    def add(self, product):
+        product_id = str(product.id)
+
+        #Logic
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] ={'price': str(product.price)}
+
+        self.session.modified =True
+
